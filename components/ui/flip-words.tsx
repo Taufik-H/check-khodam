f043de7a494c7d981096a6b1e8d38cba3ee3fc7c
Taufik Hidayatulloh <44672713+Taufik-H@users.noreply.github.com>
2024-06-23 10:48:52 +0700

    Update flip-words.tsx

--- components/ui/flip-words.tsx
+++ components/ui/flip-words.tsx
@@ -1,33 +1,37 @@
 "use client";
-import React, { useCallback, useEffect, useRef, useState } from "react";
-import { AnimatePresence, motion, LayoutGroup } from "framer-motion";
+import React, { useCallback, useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 import { cn } from "@/lib/utils";
 
 export const FlipWords = ({
   words,
   duration = 3000,
   className,
+  auto = false, // Add auto prop to control automatic animation
 }: {
   words: string[];
   duration?: number;
   className?: string;
+  auto?: boolean; // Add auto prop type
 }) => {
   const [currentWord, setCurrentWord] = useState(words[0]);
   const [isAnimating, setIsAnimating] = useState<boolean>(false);
 
-  // thanks for the fix Julian - https://github.com/Julian-AT
   const startAnimation = useCallback(() => {
-    const word = words[words.indexOf(currentWord) + 1] || words[0];
-    setCurrentWord(word);
-    setIsAnimating(true);
-  }, [currentWord, words]);
+    if (auto) {
+      const word = words[words.indexOf(currentWord) + 1] || words[0];
+      setCurrentWord(word);
+      setIsAnimating(true);
+    }
+  }, [currentWord, words, auto]);
 
   useEffect(() => {
-    if (!isAnimating)
+    if (auto && !isAnimating) {
       setTimeout(() => {
         startAnimation();
       }, duration);
-  }, [isAnimating, duration, startAnimation]);
+    }
+  }, [isAnimating, duration, startAnimation, auto]);
 
   return (
     <AnimatePresence
